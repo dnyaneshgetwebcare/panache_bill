f3a520c23d2ea719209587c25e6b77bd62d64df9
Dnyanesh Off <dnyaneshwareh@gmail.com>
2022-07-30 12:24:41 +0530

    Add missing config

---
+++ common/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=panache_final',
+            'username' => 'root',
+            //'password' => 'password',
+            'password' => '7B3RUJHdAapCFDcA',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
