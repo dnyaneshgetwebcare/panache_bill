c9830c648c35886633a9372ee073b933f07855d9
desk014 <dnyaneshwareh@gmail.com>
2025-02-25 17:45:45 +0530

    Design changed

--- backend/views/booking/item_select.php
+++ backend/views/booking/item_select.php
@@ -1,4 +1,18 @@
 <div class="row">
+
+  <div class="flex-wrap gap-2">
+  <?php
+  foreach ($item_category_list as $category) {
+    ?>
+
+    <span class="badge rounded-pill border border-primary text-primary pill-btn bg-white cat-badge" ><?=
+      $category->name;
+    ?></span>
+
+  <?php
+  }
+  ?>
+    </div>
   <?php
  // echo "<pre>"; print_r($item_master);die;
   foreach ($item_category_list as $category) {
@@ -13,8 +27,6 @@
   foreach ($items_list as $item) {
     if ($item['images']) {
                       $image_path = Yii::getAlias('@web') . '/uploads/' . $item['images'];
-
-
                     } else {
                       $image_path = Yii::getAlias('@web') . '/img/no-image.jpg';
                     }
@@ -24,7 +36,8 @@
 		<label class="imagecheck mb-2">
 			<input name="imagecheck" type="checkbox" value="<?= $item['id'] ?>>" class="imagecheck-input">
 			<figure class="imagecheck-figure">
-				<img src="<?= $image_path ?>" alt="title" class="imagecheck-image" style="height: 150px; width: 150px">
+				<img src="<?= $image_path ?>" alt="title" class="imagecheck-image" style="height: 150px; width: 150px;
+				min-height: 150px; min-width: 150px">
 			</figure>
 		</label>
 	</div>
@@ -34,3 +47,12 @@
   } ?>
 
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).on("click", ".cat-badge", function () {
+      console.log("test")
+        //$(this).toggleClass("active bg-dark text-white");
+      $(this).addClass("bg-white text-black")
+      $(this).removeClass("bg-primary")
+    });
+</script>
