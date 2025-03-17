4358a65acd0d457acf5422318a764dcdc678705a
Dnyanesh Off <dnyaneshwareh@gmail.com>
2025-03-17 19:22:38 +0530

    Design changed

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace frontend\controllers;
 
 use backend\models\BookingItem;
@@ -24,251 +25,257 @@ use frontend\models\ContactForm;
  */
 class SiteController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout', 'signup', 'about', 'contact', 'index'],
-                'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'actions' => ['logout', 'about',  'index'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
+  /**
+   * {@inheritdoc}
+   */
+  public function behaviors()
+  {
+    return [
+      'access' => [
+        'class' => AccessControl::className(),
+        'only' => ['logout', 'signup', 'about', 'contact', 'index'],
+        'rules' => [
+          [
+            'actions' => ['signup'],
+            'allow' => true,
+            'roles' => ['?'],
+          ],
+          [
+            'actions' => ['logout', 'about', 'index'],
+            'allow' => true,
+            'roles' => ['@'],
+          ],
+        ],
+      ],
+      'verbs' => [
+        'class' => VerbFilter::className(),
+        'actions' => [
+          'logout' => ['post'],
+        ],
+      ],
+    ];
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function actions()
+  {
+    return [
+      'error' => [
+        'class' => 'yii\web\ErrorAction',
+      ],
+      'captcha' => [
+        'class' => 'yii\captcha\CaptchaAction',
+        'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+      ],
+    ];
+  }
+
+  /**
+   * Displays homepage.
+   *
+   * @return mixed
+   */
+  public function actionIndex()
+  {
+    return $this->render('index');
+  }
+
+  /**
+   * Logs in a user.
+   *
+   * @return mixed
+   */
+  public function actionLogin()
+  {
+    if (!Yii::$app->user->isGuest) {
+      return $this->goHome();
     }
 
+    $model = new LoginForm();
+    if ($model->load(Yii::$app->request->post()) && $model->login()) {
+      return $this->goBack();
+    } else {
+      $model->password = '';
 
-    /**
-     * {@inheritdoc}
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
+      return $this->renderPartial('login', [
+        'model' => $model,
+      ]);
     }
+  }
 
-    /**
-     * Displays homepage.
-     *
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+  /**
+   * Logs out the current user.
+   *
+   * @return mixed
+   */
+  public function actionLogout()
+  {
+    Yii::$app->user->logout();
 
-    /**
-     * Logs in a user.
-     *
-     * @return mixed
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        } else {
-            $model->password = '';
-
-            return $this->renderPartial('login', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Logs out the current user.
-     *
-     * @return mixed
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
+    return $this->goHome();
+  }
 
   public function actionItemList()
   {
 
+  }
+
+  /**
+   * Displays contact page.
+   *
+   * @return mixed
+   */
+  public function actionContact()
+  {
+    $model = new ContactForm();
+    if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+      if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
+        Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
+      } else {
+        Yii::$app->session->setFlash('error', 'There was an error sending your message.');
+      }
+
+      return $this->refresh();
+    } else {
+      return $this->render('contact', [
+        'model' => $model,
+      ]);
     }
-    /**
-     * Displays contact page.
-     *
-     * @return mixed
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
-                Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
-            } else {
-                Yii::$app->session->setFlash('error', 'There was an error sending your message.');
-            }
+  }
 
-            return $this->refresh();
-        } else {
-            return $this->render('contact', [
-                'model' => $model,
-            ]);
-        }
-    }
+  /**
+   * Displays about page.
+   *
+   * @return mixed
+   */
+  public function actionAbout()
+  {
+    $items = [92, 20, 25, 15, 30];
+    $itemmaster = ItemMaster::find()->where(['id' => $items])->asArray()->all();
+    $item_by_type = ArrayHelper::index($itemmaster, null, 'category_id');
+    $mens = isset($item_by_type[1]) ? count($item_by_type[1]) : 0;
+    $womens = isset($item_by_type[2]) ? count($item_by_type[2]) : 0;
+    $jewellary = isset($item_by_type[3]) ? count($item_by_type[3]) : 0;
+    $booking_items = BookingItem::find()->select(['booking_header.booking_date', 'booking_header.pickup_date', 'booking_header.return_date', 'booking_header.customer_id', 'booking_header.order_status', 'product_id', 'customer_master.name as customer_name', 'booking_item.amount', 'booking_item.discount',  'booking_item.earning_amount', 'booking_header.status'  ])->leftJoin('booking_header', 'booking_header.booking_id = booking_item.booking_id')->leftJoin('customer_master', 'booking_header.customer_id = customer_master.id')->where(['product_id' => $items, 'booking_header.order_status' =>['Open', 'Closed']])->createCommand()->queryAll();
+    $booking_details = ArrayHelper::index($booking_items, null, 'product_id');
 
-    /**
-     * Displays about page.
-     *
-     * @return mixed
-     */
-    public function actionAbout()
-    {
-      $items = [92, 20, 25 ,15 , 30];
-      $itemmaster = ItemMaster::find()->where(['id' => $items])->asArray()->all();
-      $booking_items = BookingItem::find()->select(['booking_header.booking_date', 'booking_header.pickup_date', 'booking_header.return_date', 'booking_header.customer_id', 'booking_header.order_status',	'product_id', 'customer_master.name as customer_name'])->leftJoin('booking_header','booking_header.booking_id = booking_item.booking_id')->leftJoin('customer_master','booking_header.customer_id = customer_master.id')->where(['product_id'=>$items])->createCommand()->queryAll();
-      $booking_details = ArrayHelper::index($booking_items,null,'product_id');
+    return $this->render('about', ['item_master' => $itemmaster, 'booking_details' => $booking_details, 'mens' =>
+      $mens, 'women'=> $womens, 'jewellary' => $jewellary]);
+  }
 
-        return $this->render('about',['item_master' =>$itemmaster, 'booking_details'=> $booking_details]);
-    }
-
-    /**
-     * Signs user up.
-     *
-     * @return mixed
-     */
-    public function actionSignup()
-    {
-        $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
-            Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
-            return $this->goHome();
-        }
-
-        return $this->render('signup', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Requests password reset.
-     *
-     * @return mixed
-     */
-    public function actionRequestPasswordReset()
-    {
-        $model = new PasswordResetRequestForm();
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            if ($model->sendEmail()) {
-                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
-
-                return $this->goHome();
-            } else {
-                Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for the provided email address.');
-            }
-        }
-
-        return $this->render('requestPasswordResetToken', [
-            'model' => $model,
-        ]);
+  /**
+   * Signs user up.
+   *
+   * @return mixed
+   */
+  public function actionSignup()
+  {
+    $model = new SignupForm();
+    if ($model->load(Yii::$app->request->post()) && $model->signup()) {
+      Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
+      return $this->goHome();
     }
 
-    /**
-     * Resets password.
-     *
-     * @param string $token
-     * @return mixed
-     * @throws BadRequestHttpException
-     */
-    public function actionResetPassword($token)
-    {
-        try {
-            $model = new ResetPasswordForm($token);
-        } catch (InvalidArgumentException $e) {
-            throw new BadRequestHttpException($e->getMessage());
-        }
+    return $this->render('signup', [
+      'model' => $model,
+    ]);
+  }
 
-        if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
-            Yii::$app->session->setFlash('success', 'New password saved.');
+  /**
+   * Requests password reset.
+   *
+   * @return mixed
+   */
+  public function actionRequestPasswordReset()
+  {
+    $model = new PasswordResetRequestForm();
+    if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+      if ($model->sendEmail()) {
+        Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
 
-            return $this->goHome();
-        }
-
-        return $this->render('resetPassword', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Verify email address
-     *
-     * @param string $token
-     * @throws BadRequestHttpException
-     * @return yii\web\Response
-     */
-    public function actionVerifyEmail($token)
-    {
-        try {
-            $model = new VerifyEmailForm($token);
-        } catch (InvalidArgumentException $e) {
-            throw new BadRequestHttpException($e->getMessage());
-        }
-        if ($user = $model->verifyEmail()) {
-            if (Yii::$app->user->login($user)) {
-                Yii::$app->session->setFlash('success', 'Your email has been confirmed!');
-                return $this->goHome();
-            }
-        }
-
-        Yii::$app->session->setFlash('error', 'Sorry, we are unable to verify your account with provided token.');
         return $this->goHome();
+      } else {
+        Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for the provided email address.');
+      }
     }
 
-    /**
-     * Resend verification email
-     *
-     * @return mixed
-     */
-    public function actionResendVerificationEmail()
-    {
-        $model = new ResendVerificationEmailForm();
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            if ($model->sendEmail()) {
-                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
-                return $this->goHome();
-            }
-            Yii::$app->session->setFlash('error', 'Sorry, we are unable to resend verification email for the provided email address.');
-        }
+    return $this->render('requestPasswordResetToken', [
+      'model' => $model,
+    ]);
+  }
 
-        return $this->render('resendVerificationEmail', [
-            'model' => $model
-        ]);
+  /**
+   * Resets password.
+   *
+   * @param string $token
+   * @return mixed
+   * @throws BadRequestHttpException
+   */
+  public function actionResetPassword($token)
+  {
+    try {
+      $model = new ResetPasswordForm($token);
+    } catch (InvalidArgumentException $e) {
+      throw new BadRequestHttpException($e->getMessage());
     }
+
+    if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
+      Yii::$app->session->setFlash('success', 'New password saved.');
+
+      return $this->goHome();
+    }
+
+    return $this->render('resetPassword', [
+      'model' => $model,
+    ]);
+  }
+
+  /**
+   * Verify email address
+   *
+   * @param string $token
+   * @return yii\web\Response
+   * @throws BadRequestHttpException
+   */
+  public function actionVerifyEmail($token)
+  {
+    try {
+      $model = new VerifyEmailForm($token);
+    } catch (InvalidArgumentException $e) {
+      throw new BadRequestHttpException($e->getMessage());
+    }
+    if ($user = $model->verifyEmail()) {
+      if (Yii::$app->user->login($user)) {
+        Yii::$app->session->setFlash('success', 'Your email has been confirmed!');
+        return $this->goHome();
+      }
+    }
+
+    Yii::$app->session->setFlash('error', 'Sorry, we are unable to verify your account with provided token.');
+    return $this->goHome();
+  }
+
+  /**
+   * Resend verification email
+   *
+   * @return mixed
+   */
+  public function actionResendVerificationEmail()
+  {
+    $model = new ResendVerificationEmailForm();
+    if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+      if ($model->sendEmail()) {
+        Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
+        return $this->goHome();
+      }
+      Yii::$app->session->setFlash('error', 'Sorry, we are unable to resend verification email for the provided email address.');
+    }
+
+    return $this->render('resendVerificationEmail', [
+      'model' => $model
+    ]);
+  }
 }
--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -28,8 +28,8 @@ $this->params['breadcrumbs'][] = $this->title;
                       </div>
                       <div class="col-7 col-stats">
                         <div class="numbers">
-                          <p class="card-category">Number</p>
-                          <h4 class="card-title">150GB</h4>
+                          <p class="card-category">Women</p>
+                          <h4 class="card-title"><?= $women; ?></h4>
                         </div>
                       </div>
                     </div>
@@ -47,8 +47,8 @@ $this->params['breadcrumbs'][] = $this->title;
                       </div>
                       <div class="col-7 col-stats">
                         <div class="numbers">
-                          <p class="card-category">Revenue</p>
-                          <h4 class="card-title">$ 1,345</h4>
+                          <p class="card-category">Men</p>
+                          <h4 class="card-title"><?= $mens ?></h4>
                         </div>
                       </div>
                     </div>
@@ -66,37 +66,26 @@ $this->params['breadcrumbs'][] = $this->title;
                       </div>
                       <div class="col-7 col-stats">
                         <div class="numbers">
-                          <p class="card-category">Errors</p>
-                          <h4 class="card-title">23</h4>
+                          <p class="card-category">Jewellary</p>
+                          <h4 class="card-title"><?= $jewellary ?></h4>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="col-sm-6 col-md-3">
-                <div class="card card-stats card-round">
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col-5">
-                        <div class="icon-big text-center">
-                          <i class="icon-social-twitter text-primary"></i>
-                        </div>
-                      </div>
-                      <div class="col-7 col-stats">
-                        <div class="numbers">
-                          <p class="card-category">Followers</p>
-                          <h4 class="card-title">+45K</h4>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+
             </div>
 
   <div class="row">
-    <?php foreach ($item_master as $item) { ?>
+    <?php
+    foreach ($item_master as $item) {
+      $rent_amount = 0;
+    $number_of_times = 0;
+    $upcoming_booking = 0;
+    $upcoming_booking_times = 0;
+
+      ?>
     <div class="col-md-4">
                 <div class="card card-profile">
                   <div class="card-header" style="background-image: url('assets/img/blogpost.jpg')">
@@ -107,23 +96,36 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
 
                     <div class="title">Booking</div>
-                    <div class="card-list" style="overflow: scroll;max-height: 300px; min-height: 300px">
+                    <div class="card-list py-4" style="overflow: auto;max-height: 300px; min-height: 300px">
 
                         <?php
                         if(isset($booking_details[$item['id']])){
                         $product_bookings = $booking_details[$item['id']];
                       //  echo "<pre>"; print_r($booking_details);die;
+
                         foreach ($product_bookings as $booking) {
+                          $actual_rent = $booking['amount'];
+                          if($booking['status'] == 'Returned'){
+                            $rent_amount += $actual_rent;
+                            $number_of_times++;
+                          }else{
+                            $upcoming_booking +=$actual_rent;
+                            $upcoming_booking_times++;
+                          }
 
                           ?>
-                            <div class="item-list">
-                        <div class="info-user ms-3">
-                          <div class="username"><?= $booking['customer_name']; ?></div>
-                          <div class="status"><?= Yii::$app->formatter->asDate($booking['pickup_date'],'d-MM-yy');
+                          <div class="item-list">
+											<div class="avatar">
+												<span class="round">14/Apr</span>
+											</div>
+											<div class="info-user ms-3">
+												<div class="username"><?= $booking['customer_name']; ?></div>
+												<div class="status"><?= Yii::$app->formatter->asDate($booking['pickup_date'],'d-MM-yy');
                           ?> >>  <?= Yii::$app->formatter->asDate($booking['pickup_date'],'d-MM-yy');
                           ?></div>
-                        </div>
-                                </div>
+											</div>
+                            <span> <?= thousandsCurrencyFormat($actual_rent) ?> </span>
+										</div>
                         <?php }
                         }else{
                           echo "NO Items";
@@ -137,16 +139,16 @@ $this->params['breadcrumbs'][] = $this->title;
                   <div class="card-footer">
                     <div class="row user-stats text-center">
                       <div class="col">
-                        <div class="number">125</div>
-                        <div class="title">Post</div>
+                        <div class="number"><?= $number_of_times ?></div>
+                        <div class="title">Nos Times</div>
                       </div>
                       <div class="col">
-                        <div class="number">25K</div>
-                        <div class="title">Followers</div>
+                        <div class="number"><?= thousandsCurrencyFormat($rent_amount)  ?></div>
+                        <div class="title">Total Earned</div>
                       </div>
                       <div class="col">
-                        <div class="number">134</div>
-                        <div class="title">Following</div>
+                        <div class="number"><?= $upcoming_booking  ?></div>
+                        <div class="title">Upcoming</div>
                       </div>
                     </div>
                   </div>
@@ -155,3 +157,22 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php } ?>
   </div>
 </div>
+<?php function thousandsCurrencyFormat($num) {
+
+  if($num>1000) {
+
+        $x = round($num);
+        $x_number_format = number_format($x);
+        $x_array = explode(',', $x_number_format);
+        $x_parts = array('k', 'm', 'b', 't');
+        $x_count_parts = count($x_array) - 1;
+        $x_display = $x;
+        $x_display = $x_array[0] . ((int) $x_array[1][0] !== 0 ? '.' . $x_array[1][0] : '');
+        $x_display .= $x_parts[$x_count_parts - 1];
+
+        return $x_display;
+
+  }
+
+  return $num;
+}  ?>
