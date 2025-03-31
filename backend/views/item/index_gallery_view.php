6010a795d330040b5644276e0be45f59f4ec49e4
desk014 <dnyaneshwareh@gmail.com>
2025-03-31 17:47:00 +0530

    Design changed

--- backend/views/item/index_gallery_view.php
+++ backend/views/item/index_gallery_view.php
@@ -9,6 +9,15 @@ $this->title = 'Item Masters';
       position: absolute;
     right: 20px;
   }
+  .item_rent{
+      position: absolute;
+    right: 20px;
+    text-align: center;
+    bottom: 74px;
+    width: 70%;
+    background-color: #726f6f70;
+    color: #ffffff;
+  }
 </style>
 <div class="row">
   <div class="col-md-12">
@@ -21,15 +30,20 @@ $this->title = 'Item Masters';
                                         echo DatePicker::widget([
                                             'name' => 'filter_from_date',
                                             'name2' => 'filter_from_date',
-
+                                            'attribute' => 'from_date',
+                                          'attribute2' => 'to_date',
                                             'type' => DatePicker::TYPE_RANGE,
                                             //'value' => $model['booking_date'],
                                             //'disabled' =>($readonly_GOODS_header)?$readonly_GOODS_header:$readonly_closed_string,
                                             'options' => [
                                                 'placeholder' => 'dd-mm-yyyy',
                                                 'autocomplete' => 'off',
-                                              'placeholder2' => 'dd-mm-yyyy',
-                                                'autocomplete2' => 'off'
+
+                                            ],
+                                            'options2' => [
+                                                'placeholder' => 'dd-mm-yyyy',
+                                                'autocomplete' => 'off',
+
                                             ],
                                             'pluginOptions' => [
                                                 'autoclose' => true,
@@ -49,6 +63,7 @@ $this->title = 'Item Masters';
                   <img src="<?= $item_details->imageurl; ?>" class="img-fluid" style="max-height: 200px;
 											min-height: 200px; align-content: center">
                 </a>
+                <span class="item_rent"> ₹ <?= $item_details->rent_amount; ?></span>
               </div>
               <div class="card-footer" style="display: flex; flex-direction: row;">
                 <span class="text-bold col-9">
